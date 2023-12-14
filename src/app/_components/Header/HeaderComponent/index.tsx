47f6520b4c9be62744455659b562e5ab5ef43c23
Centeno Jose Antonio <jocecito182@gmail.com>
2023-12-14 18:21:21 -0300

    First Commit

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import HeaderMobileNav from '../HeaderMobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav className={[classes.header]}>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="Logo" width={170} height={50} />
+        </Link>
+
+        <HeaderNav header={header} />
+        <HeaderMobileNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
---
+++ src/app/_components/Header/HeaderMobileNav/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '../../../../payload/payload-types'
+
+const HeaderMobileNav = ({ header }: { header: Header }) => {
+  return <div>HeaderMobileNav</div>
+}
+
+export default HeaderMobileNav
