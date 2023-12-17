34ae33d98912d342409b7710956867a0b5e886ea
Centeno Jose Antonio <jocecito182@gmail.com>
2023-12-16 21:16:03 -0300

    Edit all login pages

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { Gutter } from '../../_components/Gutter'
+import { RenderParams } from '../../_components/RenderParams'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
@@ -9,9 +12,33 @@ import classes from './index.module.scss'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <Link href="/login" className={classes.backLink}>
+            <p>👈🏻 Back</p>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Forgot Password</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
